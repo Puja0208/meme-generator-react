ef957bd296f809d0a10903e5c7cde4c15276fc2a
Puja0208 <pooja2081997@gmail.com>
2021-08-17 09:39:04 +0530

    3 files modified, 2 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
 
+import Header from './components/Header';
+import MemeGenerator from './components/MemeGenerator';
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Header />
+      <MemeGenerator />
     </div>
   );
 }
